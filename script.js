ed6daf58857721c33046d055174e28cd25b97529
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-30 15:54:50 +0200

    feat: almost there, got the values

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const main = document.querySelector("main");
+let main = document.querySelector("main");
+
+console.log(main);
+let redValues = red.getAttribute("value");
+let greenValues = green.getAttribute("value");
+let blueValues = blue.getAttribute("value");
+
+let rgb = "rgb(" + redValues + "," + greenValues + "," + blueValues + ")";
+
+console.log(rgb);
