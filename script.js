3cefbca92f6e83b57d5270e9a0114255250d6f84
Mark Grimm <mark.grimm.coding@gmail.com>
2022-07-11 15:30:29 +0200

    feat: adds random color button

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function changeColor() {
 document.querySelector("#red").addEventListener("input", changeColor);
 document.querySelector("#green").addEventListener("input", changeColor);
 document.querySelector("#blue").addEventListener("input", changeColor);
+
+let rnd = document.querySelector(".rnd");
+
+rnd.addEventListener("click", () => {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => {
+      return response.json();
+    })
+    .then((colorObject) => {
+      document.querySelector("p").innerText =
+        colorObject.rgb.r + "," + colorObject.rgb.g + "," + colorObject.rgb.b;
+      let = redValues = String(colorObject.rgb.r);
+      let = greenValues = String(colorObject.rgb.g);
+      let = blueValues = String(colorObject.rgb.b);
+      let rgb = "rgb(" + redValues + "," + greenValues + "," + blueValues + ")";
+      document.body.style.backgroundColor = rgb;
+    });
+});
